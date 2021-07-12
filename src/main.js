2621357f25b941647ae7127d7cc92e93d7dc25de
David Prada Linarez <davidprali27@gmail.com>
2021-07-12 08:00:17 -0500

    travisci

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 //importamos la funcion router enviada desde routers/index.js y este se ejecutara en este archivo
-import router from "./routers";
+import router from "./routers/index.js";
 
 //Aca se encuentran todos nuestros estilos, empezando desde el global hasta los modularizados o de cada pagina.
 import "./style.css";
@@ -14,10 +14,6 @@ import "./css/previusClases.css";
 //este es el estilo del template header.
 import "./css/header.css";
 import "./css/footer.css";
-
-import "./models/Curso.js";
-import "./models/Clase.js";
-import "./models/Usuario.js";
 
 //llamamos window.addEventListener para escuchar al evento cargar, osea escuchar cuando recarga la ventana del navegador(refresh)
 window.addEventListener("load", () => {
